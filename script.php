250de24ba88fcb03d1308a36bc3f1f86c322bdc5
oleg <sad.net79@gmail.com>
2021-05-06 13:12:38 +0300

    version>1.0

---
+++ script.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package    import_dependent_attr
+ *
+ * @author     oleg <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Import_dependent_attr script file.
+ *
+ * @package   import_dependent_attr
+ * @since     1.0.0
+ */
+class plgJshoppingadminImport_dependent_attrInstallerScript
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 */
+	public function __construct(JAdapterInstance $adapter) {}
+
+	/**
+	 * Called before any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function preflight($route, JAdapterInstance $adapter) {}
+
+	/**
+	 * Called after any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function postflight($route, JAdapterInstance $adapter) {}
+
+	/**
+	 * Called on installation
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function install(JAdapterInstance $adapter) {}
+
+	/**
+	 * Called on update
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function update(JAdapterInstance $adapter) {}
+
+	/**
+	 * Called on uninstallation
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 */
+	public function uninstall(JAdapterInstance $adapter) {}
+}
